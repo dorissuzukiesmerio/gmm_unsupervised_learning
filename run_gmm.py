8f8f41f162826d6aab88e59e124cc6eda3f10689
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-11-04 16:14:28 -0400

    scatterplot and modifying the data

--- run_gmm.py
+++ run_gmm.py
@@ -1,1 +1,18 @@
-print("hahah")
+import pandas
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as pyplot
+
+data = pandas.read_csv("dataset.csv")
+
+print(data) # we see that the code had an extra unneeded column 
+
+print(data.columns)
+data.drop('Unnamed: 0', axis=1, inplace = True) # dropping the first element of the axis = column 
+print(data)
+
+data = data.rename({0:'index', 'x1':'data1', 'x2':'data2'}, inplace= True)
+
+data = data.values
+
+pyplot.scatter(data[:,1], data[:,2])
+pyplot.savefig("scatter.png") # So we see that the distribution inside the group is different -> KMeans will not work as well
